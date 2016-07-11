201906b3c504a4d16b8279f7826274d5f9b1a22d
Johannes Hund <johannes.hund@siemens.com>
2016-07-11 15:48:44 +0800

    added a bunch of scripts for backup

---
+++ beijing072016/remote_led.js
@@ -0,0 +1,51 @@
+//just an example script - to be moved into other repo
+
+// this script reacts to changes in the votes and changes the color of the LED accordingly
+
+var basicLed = null;
+var voter = null;
+
+console.log("looking for voter locally");
+
+ WoT.discover('local', { 'name' : 'voter'})
+ .then(function(things) {
+    console.log("discover returned: " + things);
+    voter = things[0];
+
+    console.log("first element: " + voter);
+
+    //should be only one thing
+    console.log("found " + voter.name);
+
+    WoT.consumeDescriptionUri("http://192.168.0.185:8080/things/basicLed").then(function(basicLed) {
+        console.log("discover of basicLed returned: " + basicLed);
+
+         voter.onUpdateProperty('votes', function(votes) {
+             if(votes < 0) {
+                 // make led blue
+                 console.log("setting remote LED blue");
+                 basicLed.setProperty("rgbValueRed",0);
+                 basicLed.setProperty("rgbValueGreen",0);
+                 basicLed.setProperty("rgbValueBlue",120);
+                 basicLed.setProperty("brightness",40);
+             } else if(votes > 0) {
+                 // make led red
+                 console.log("setting remote LED red");
+                 basicLed.setProperty("rgbValueRed",255);
+                 basicLed.setProperty("rgbValueGreen",0);
+                 basicLed.setProperty("rgbValueBlue",0);
+             }  else  { // (votes == 0)
+                 // make led white
+                 console.log("setting remote LED off");
+                 basicLed.setProperty("rgbValueRed",60);
+                 basicLed.setProperty("rgbValueGreen",60);
+                 basicLed.setProperty("rgbValueBlue",60);
+             };
+         });
+    })
+ })
+._catch(function(err){
+    console.error(err);
+});
+
+console.log("client script running");
---
+++ beijing072016/voter.js
@@ -0,0 +1,38 @@
+ //just an example script - to be moved into other repo
+
+  WoT.discover('local', { 'name' : 'voter'})
+ .then(function(things) {
+    console.log("discover returned: " + things);
+    voter = things[0];
+
+    console.log("first element: " + voter);
+
+    //should be only one thing
+    console.log("found " + voter.name);
+        voter.setProperty("votes",0);
+        voter.onUpdateProperty("votes",
+                function(newValue, oldValue) {
+                    console.log(oldValue + " -> " + newValue);
+                    var message = (oldValue < newValue)? "increased " : "decreased";
+                    console.log("votes " + message + " to " + newValue);
+                }
+             );
+
+             voter
+             .onInvokeAction("tooCold", function() {
+                console.log("vote for too cold, incrementing votes");
+                var value = voter.getProperty("votes") + 1;
+                if(value >= 10) value = 10;
+                voter.setProperty("votes", value);
+                return value;
+             });
+
+              voter
+              .onInvokeAction("tooHot", function() {
+                 console.log("vote for too hot, decrementing votes");
+                 var value = voter.getProperty("votes") - 1;
+                 if(value <= -10) value = -10;
+                 voter.setProperty("votes", value);
+                 return value;
+              });
+    });
